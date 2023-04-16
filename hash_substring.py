3b45578c40c47562e37bf0fbc5074db7c26463cd
Sintija Norkārkle <124626861+SintijaNorkarkle@users.noreply.github.com>
2023-04-16 15:15:04 +0000

    Pēdējais variants

--- hash_substring.py
+++ hash_substring.py
@@ -1,3 +1,7 @@
+"""
+Sintija Norkārkle, RDCPO, 1. grupa
+"""
+
 # python3
 
 def read_input():
@@ -8,7 +12,9 @@ def read_input():
     # read two lines 
     # first line is pattern 
     # second line is text in which to look for pattern 
+    
     source = input()
+    
     if "I" in source:
         pattern = input()
         text = input()
@@ -16,11 +22,11 @@ def read_input():
         with open("./tests/06", mode = "r") as fails:
             pattern = fails.readline()
             text = fails.readline()
+    # ja lietotājs norādīja nederīgu ievades avotu, tiek izdrukāts kļūdas paziņojums
     else :
         print("Invalid input source")
 
     # return both lines in one return
-    
     # this is the sample return, notice the rstrip function
     return (pattern.rstrip(), text.rstrip())
 
@@ -32,30 +38,36 @@ def print_occurrences(output):
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
     
+    # ja meklējamās rakstzīmju virknes garums ir lielāks par teksta garumu, tad atgriežam tukšu sarakstu
     if len(pattern) > len(text):
         return []
     
+    # definēti mainīgie, kuri tiks izmantoti vēlāk
     hashmod = 10**9 + 7
     multiplier = 31
     text_length = len(text)
     pattern_length = len(pattern)
-
     rm = pow(multiplier, pattern_length - 1, hashmod)
     p_hash = t_hash = 0
 
+    # tiek aprēķinātas hash vērtības gan meklējamai rakstzīmju virknei, gan pirmajai tekstā sastopamajai virknei
     for i in range (pattern_length) :
         p_hash = (pow(multiplier, 1, hashmod) * p_hash + ord(pattern[i])) % hashmod
         t_hash = (pow(multiplier, 1, hashmod) * t_hash + ord(text[i])) % hashmod
 
+    # tiek izveidots tukšs saraksts, kurā tiks saglabātas meklējamo rakstzīmju virkņu atrašanās vietas tekstā
     occurrences = []
 
+    # tiek salīdzinātas hash vērtības, lai atrastu visus vienādos fragmentus, kurus pēc tam pievieno sarakstam
     for i in range (text_length - pattern_length + 1):
         if hash(pattern) == hash(text[i : i + pattern_length]):
             occurrences.append(i)
 
+    # ja visa virkne nav pārbaudīta, tad turpina to darīt un pārbauda, vai ir vēl kāda vieta, kur ir vienādi fragmenti
     if i < text_length - pattern_length:
         t_hash = ((multiplier * t_hash) % hashmod - (multiplier * ord(text[i] * rm) % hashmod + ord(text[i + pattern_length]))) % hashmod
 
+    # ja hash vērtība ir negatīva, tad tai jāpieskaita hashmod, lai iegūtu pozitīvu vērtību
         if t_hash < 0 :
             t_hash += hashmod
     
